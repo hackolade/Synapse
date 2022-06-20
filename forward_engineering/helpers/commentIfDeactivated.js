09680f32de6d34ac0098467c7d1eb20b0aec9481
DrMyroslav <myroslav.druchkiv@binary-studio.com>
2022-06-20 18:57:17 +0300

    fix filter

--- forward_engineering/helpers/commentIfDeactivated.js
+++ forward_engineering/helpers/commentIfDeactivated.js
@@ -1,5 +1,4 @@
 const BEFORE_DEACTIVATED_STATEMENT = '-- ';
-const REQ_MULTY_LINE_COMMENT = /\/\*[\s\S]*?\*\//gi;
 
 const commentIfDeactivated = (statement, data, isPartOfLine) => {
 	if (data?.hasOwnProperty('isActivated') && !data.isActivated) {
@@ -18,8 +17,12 @@ const queryIsDeactivated = (query, statement) => {
 	return query.includes(statement) && query.startsWith(BEFORE_DEACTIVATED_STATEMENT);
 };
 
-const filterDeactivatedQuery = query => {
-	return query.replace(REQ_MULTY_LINE_COMMENT, '');
+const createRegForMultyLineComment = terminator => 
+	new RegExp(`\\/\\*[\\s\\S]*?${terminator}\n?\\s\\*\\/`, 'gi');
+
+const filterDeactivatedQuery = (query, terminator) => {
+	const regExp = createRegForMultyLineComment(terminator);
+	return query.replace(regExp, '');
 };
 
 module.exports = { 
