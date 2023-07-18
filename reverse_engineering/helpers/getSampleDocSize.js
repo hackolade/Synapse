1118465dcc46039b2325ed0081e7f87591954266
Danylo Strelets <danylo.strelets@hackolade.com>
2023-07-18 18:08:30 +0300

    HCK-3553: remove unnecessary statement

--- reverse_engineering/helpers/getSampleDocSize.js
+++ reverse_engineering/helpers/getSampleDocSize.js
@@ -1,8 +1,4 @@
 const getSampleDocSize = (count, recordSamplingSettings) => {
-	if (recordSamplingSettings.active === 'absolute') {
-		return Number(recordSamplingSettings.absolute.value);
-	}
-
 	const limit = Math.ceil((count * recordSamplingSettings.relative.value) / 100);
 
 	return Math.min(limit, recordSamplingSettings.maxValue);
