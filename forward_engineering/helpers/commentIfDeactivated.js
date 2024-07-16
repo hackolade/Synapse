4bd416478bd0304107b092b439c7f0fa481c9c9c
Alik <alik.rahmonov@gmail.com>
2024-07-16 16:24:24 +0300

    fix: regular expressions are invalid (#74)

--- forward_engineering/helpers/commentIfDeactivated.js
+++ forward_engineering/helpers/commentIfDeactivated.js
@@ -1,5 +1,5 @@
 const BEFORE_DEACTIVATED_STATEMENT = '-- ';
-const REG_FOR_MULTYLINE_COMMENT = /(\n\/\*\n[\s\S]*?\n\s\*\/\n)$|(\n\/\*\n[\s\S]*?\n\s\*\/)$/gi;
+const REG_FOR_MULTYLINE_COMMENT = /(\n\/\*\n[\s\S]*?\n\s\*\/\n)|((\n\/\*\n[\s\S]*?\n\s\*\/)$)/gi;
 
 const commentIfDeactivated = (statement, data, isPartOfLine) => {
 	if (data?.hasOwnProperty('isActivated') && !data.isActivated) {
--- reverse_engineering/reverseEngineeringService/helpers/reverseTableColumn.js
+++ reverse_engineering/reverseEngineeringService/helpers/reverseTableColumn.js
@@ -9,8 +9,8 @@ const handleDefault = (typeObject, value) => {
 	const validValue =
 		{
 			numeric: Number(value.replace(/(^\(\()|(\)\)$)/g, '')),
-			char: value.replace(/(^\(')$|('\))$/g, ''),
-			xml: value.replace(/(^\(N')$|('\))$/g, ''),
+			char: value.replace(/(^\(')|(('\))$)/g, ''),
+			xml: value.replace(/(^\(N')|(('\))$)/g, ''),
 		}[typeObject.type] || value;
 
 	return { default: validValue };
@@ -59,7 +59,7 @@ const handleColumnProperty = (column, propertyName, value) => {
 		case 'COLUMN_DEFAULT':
 			return handleDefault(handleType(column['DATA_TYPE']), value);
 		case 'IS_NULLABLE':
-			return { required: value === 'NO' ? true : false };
+			return { required: value === 'NO' };
 		case 'DATETIME_PRECISION':
 			return { fractSecPrecision: !isNaN(value) ? value : '' };
 		case 'NUMERIC_SCALE':
