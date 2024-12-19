0df56d0a377bdc7128de652815fd62e4e2ebb162
Alik Rakhmonov <alik.rahmonov@gmail.com>
2024-12-19 16:36:15 +0100

    fix: comment out if isActivated is FALSE

--- forward_engineering/helpers/commentIfDeactivated.js
+++ forward_engineering/helpers/commentIfDeactivated.js
@@ -2,7 +2,7 @@ const BEFORE_DEACTIVATED_STATEMENT = '-- ';
 const REG_FOR_MULTYLINE_COMMENT = /(\n\/\*\n[\s\S]*?\n\s\*\/\n)|((\n\/\*\n[\s\S]*?\n\s\*\/)$)/gi;
 
 const commentIfDeactivated = (statement, data, isPartOfLine) => {
-	if (data?.hasOwnProperty('isActivated') && !data.isActivated) {
+	if (data.isActivated === false) {
 		if (isPartOfLine) {
 			return '/* ' + statement + ' */';
 		} else if (statement.includes('\n')) {
