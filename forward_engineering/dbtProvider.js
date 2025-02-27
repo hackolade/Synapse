253c0a7dad768e8cf361e2fd44a606d6aa6f78fe
Serhii Filonenko <91055067+serhii-filonenko@users.noreply.github.com>
2025-02-27 12:46:41 +0200

    HCK-10156: enable dbt feature (#104)
    * HCK-10156: add dbt provider
    
    * HCK-10156: enable dbt feature

---
+++ forward_engineering/dbtProvider.js
@@ -0,0 +1,86 @@
+/**
+ * @typedef {import('./types').ColumnDefinition} ColumnDefinition
+ * @typedef {import('./types').JsonSchema} JsonSchema
+ * @typedef {import('./types').ConstraintDto} ConstraintDto
+ */
+const { toLower, toUpper } = require('lodash');
+
+const types = require('./configs/types');
+const defaultTypes = require('./configs/defaultTypes');
+const { decorateType } = require('./helpers/columnDefinitionHelper');
+const getKeyHelper = require('./helpers/keyHelper');
+
+class DbtProvider {
+	/**
+	 * @type {AppInstance}
+	 */
+	#appInstance;
+
+	/**
+	 * @param {{ appInstance: AppInstance }}
+	 */
+	constructor({ appInstance }) {
+		this.#appInstance = appInstance;
+	}
+
+	/**
+	 * @param {{ appInstance }}
+	 * @returns {DbtProvider}
+	 */
+	static createDbtProvider({ appInstance }) {
+		return new DbtProvider({ appInstance });
+	}
+
+	/**
+	 * @param {string} type
+	 * @returns {string | undefined}
+	 */
+	getDefaultType(type) {
+		return defaultTypes[type];
+	}
+
+	/**
+	 * @returns {Record<string, object>}
+	 */
+	getTypesDescriptors() {
+		return types;
+	}
+
+	/**
+	 * @param {string} type
+	 * @returns {boolean}
+	 */
+	hasType(type) {
+		return Object.keys(types).map(toLower).includes(toLower(type));
+	}
+
+	/**
+	 * @param {{ type: string; columnDefinition: ColumnDefinition }}
+	 * @returns {string}
+	 */
+	decorateType({ type, columnDefinition }) {
+		return decorateType(toUpper(type), columnDefinition);
+	}
+
+	/**
+	 * @param {{ jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	getCompositeKeyConstraints({ jsonSchema }) {
+		const keyHelper = getKeyHelper(this.#appInstance);
+
+		return keyHelper.getCompositeKeyConstraints({ jsonSchema });
+	}
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition; jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	getColumnConstraints({ columnDefinition, jsonSchema }) {
+		const keyHelper = getKeyHelper(this.#appInstance);
+
+		return keyHelper.getColumnConstraints({ columnDefinition });
+	}
+}
+
+module.exports = DbtProvider;
