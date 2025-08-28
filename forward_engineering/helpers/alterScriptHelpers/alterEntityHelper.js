82d787f36e23ad56e1b9103a3b5b5b3e29188648
Alik Rakhmonov <alik.rahmonov@gmail.com>
2025-08-28 12:30:37 +0200

    HCK-12505: Fix order of ALTER script statements for indexes

--- forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
+++ forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
@@ -161,8 +161,8 @@ module.exports = (app, options) => {
 	return {
 		getAddCollectionScript,
 		getDeleteCollectionScript,
-		getModifyCollectionScript,
 		getAddColumnScript,
+		getModifyCollectionScript,
 		getDeleteColumnScript,
 		getModifyColumnScript,
 	};
