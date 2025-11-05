b767d2e0ba08ded4789a7f3575a08116e147b44d
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2025-11-05 18:15:26 +0200

    fix: proper import

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -2,7 +2,7 @@ const { applyToInstance } = require('./api/applyToInstance');
 const { generateContainerScript } = require('./api/generateContainerScript');
 const { generateScript } = require('./api/generateScript');
 const { generateViewScript } = require('./api/generateViewScript');
-const { getExternalBrowserUrl } = require('../reverse_engineering/api');
+const { getExternalBrowserUrl } = require('./api/getExternalBrowserUrl');
 const { isDropInStatements } = require('./api/isDropInStatements');
 const { testConnection } = require('./api/testConnection');
 
