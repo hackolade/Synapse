cc01c09c89b0ce7c3b79e2edc2dc82799bb4ccb8
Alik Rakhmonov <alik.rahmonov@gmail.com>
2026-01-20 13:36:20 +0100

    HCK-14453: Hydrate column JSON schema to resolve types

--- forward_engineering/ddlProvider.js
+++ forward_engineering/ddlProvider.js
@@ -278,6 +278,15 @@ const provider = (baseProvider, options, app) => {
 			return hasType(type);
 		},
 
+		hydrateJsonSchemaColumn(jsonSchema, definitionJsonSchema) {
+			if (!jsonSchema.$ref || _.isEmpty(definitionJsonSchema)) {
+				return jsonSchema;
+			}
+
+			jsonSchema = _.omit(jsonSchema, '$ref');
+			return { ...definitionJsonSchema, ...jsonSchema };
+		},
+
 		hydrateColumn({ columnDefinition, jsonSchema, schemaData, parentJsonSchema }) {
 			let encryption = [];
 
